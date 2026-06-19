eec847406d955ecc4f4aa61b0c2e01485e06c943
YongQuanz <yongquan6993@gmail.com>
2026-06-19 07:13:03 +0000

    first commit

---
+++ launch/so101_display.launch.py
@@ -0,0 +1,28 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg = get_package_share_directory('so101_description')
+    urdf_file = os.path.join(pkg, 'urdf', 'so101.urdf.xacro')
+
+    robot_description = Command(['xacro ', urdf_file])
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', os.path.join(pkg, 'config', 'view_robot.rviz')],
+        ),
+    ])
